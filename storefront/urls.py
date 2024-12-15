86f852a860afe9a9e20ef7267dd5c57b95aefd6f
Clair <clair.karanja@student.moringaschool.com>
2024-12-16 02:43:06 +0300

    ignore type

--- storefront/urls.py
+++ storefront/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponseRedirect
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
+from django.http import HttpResponseRedirect # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
